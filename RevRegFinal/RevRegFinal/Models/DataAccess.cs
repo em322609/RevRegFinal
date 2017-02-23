26365a19f3679244728c1ba4e00543d06d30e11f
em322609 <em322609@ohio.edu>
2017-02-23 01:03:15 -0500

    minor changes

--- RevRegFinal/RevRegFinal/Models/DataAccess.cs
+++ RevRegFinal/RevRegFinal/Models/DataAccess.cs
@@ -243,8 +243,8 @@ namespace RevRegFinal.Models
         //will add studentmodelid value & coursemodelid value into the registration table in the DB
         public static bool RegisterStudentForCourse(string courseID, int studentID)
         {
-            CourseModel c = getCourse(courseID); //Will Throw IndexOutOfRange exception if the course is not found
-           StudentModel s = getStudent(studentID); //Will Throw IndexOUtOfRange exception if the student is not found
+            CourseModel c = getCourse(courseID);
+           StudentModel s = getStudent(studentID);
 
             
             try
